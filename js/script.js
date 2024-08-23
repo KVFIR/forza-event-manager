037e6bf7340b0423621f56d0d7e3b1bd79e7fc0c
KVFIR <61096626+KVFIR@users.noreply.github.com>
2024-08-23 11:48:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@ fetch('https://forza-event-manager-5e2b3b51921f.herokuapp.com/health')
   .then(response => response.json())
   .then(data => {
     console.log('Состояние сервера:', data);
-    // Здесь можно обновить UI на основе полученных данных
-    document.getElementById('server-status').innerText = `Состояние сервера: ${data.status}`;
+    // Обновите UI на основе полученных данных
+    const statusElement = document.createElement('p');
+    statusElement.classList.add('text-center');
+    statusElement.textContent = `Состояние сервера: ${data.status}`;
+    document.querySelector('.container').appendChild(statusElement);
   })
   .catch(error => {
     console.error('Ошибка при запросе:', error);
-    document.getElementById('server-status').innerText = 'Ошибка при подключении к серверу';
+    const errorElement = document.createElement('p');
+    errorElement.classList.add('text-center', 'text-danger');
+    errorElement.textContent = 'Ошибка при подключении к серверу';
+    document.querySelector('.container').appendChild(errorElement);
   });
