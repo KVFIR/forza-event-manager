2e2d71a6bb3a62785c08894048e815774c2002dc
KVFIR <61096626+KVFIR@users.noreply.github.com>
2024-08-25 00:00:46 +0000

    Fetch user data from server using session ID

--- profile.js
+++ profile.js
@@ -1,28 +1,22 @@
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-const token = getCookie('auth_token');
-
-if (token) {
-  // Декодирование токена и отображение данных
-  const userData = JSON.parse(atob(token.split('.')[1])); // Простейшее декодирование base64
-
+fetch('https://forza-event-manager-5e2b3b51921f.herokuapp.com/user/data', {
+  method: 'GET',
+  credentials: 'include' // Для включения cookie в запросе
+})
+.then(response => response.json())
+.then(data => {
   // Отображение основной информации о пользователе
-  document.getElementById('username').innerText = userData.userInfo.username;
-  document.getElementById('avatar').src = `https://cdn.discordapp.com/avatars/${userData.userInfo.id}/${userData.userInfo.avatar}.png`;
-  document.getElementById('email').innerText = userData.userInfo.email;
+  document.getElementById('username').innerText = data.userInfo.username;
+  document.getElementById('avatar').src = `https://cdn.discordapp.com/avatars/${data.userInfo.id}/${data.userInfo.avatar}.png`;
+  document.getElementById('email').innerText = data.userInfo.email;
 
   // Отображение информации о гильдиях пользователя
   const guildsContainer = document.getElementById('guilds');
-  userData.userGuilds.forEach(guild => {
+  data.userGuilds.forEach(guild => {
       const guildElement = document.createElement('li');
       guildElement.innerText = guild.name;
       guildsContainer.appendChild(guildElement);
   });
-} else {
-  // Если токена нет, перенаправляем на страницу логина
-  window.location.href = "https://forza-event-manager-5e2b3b51921f.herokuapp.com/login";
-}
+})
+.catch(error => {
+  console.error('Ошибка при получении данных пользователя:', error);
+});
