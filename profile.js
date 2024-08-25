414f0041137559d7ee2fd5326b7b73607b27421c
KVFIR <61096626+KVFIR@users.noreply.github.com>
2024-08-25 10:45:28 +0000

    Обработка неавторизованных ответов на стороне клиента

--- profile.js
+++ profile.js
@@ -1,22 +1,39 @@
-fetch('https://forza-event-manager-5e2b3b51921f.herokuapp.com/user/data', {
-  method: 'GET',
-  credentials: 'include' // Для включения cookie в запросе
-})
-.then(response => response.json())
-.then(data => {
-  // Отображение основной информации о пользователе
-  document.getElementById('username').innerText = data.userInfo.username;
-  document.getElementById('avatar').src = `https://cdn.discordapp.com/avatars/${data.userInfo.id}/${data.userInfo.avatar}.png`;
-  document.getElementById('email').innerText = data.userInfo.email;
+// Извлечение токена из URL
+const urlParams = new URLSearchParams(window.location.search);
+const token = urlParams.get('token');
 
-  // Отображение информации о гильдиях пользователя
-  const guildsContainer = document.getElementById('guilds');
-  data.userGuilds.forEach(guild => {
-      const guildElement = document.createElement('li');
-      guildElement.innerText = guild.name;
-      guildsContainer.appendChild(guildElement);
-  });
-})
-.catch(error => {
-  console.error('Ошибка при получении данных пользователя:', error);
-});
+if (token) {
+    fetch('https://forza-event-manager-5e2b3b51921f.herokuapp.com/user/data', {
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then(response => {
+        if (!response.ok) {
+            // Если сервер вернул статус 401 или другой неудачный статус
+            return response.text().then(text => { throw new Error(text); });
+        }
+        return response.json();
+    })
+    .then(userData => {
+        // Отображение основной информации о пользователе
+        document.getElementById('username').innerText = userData.userInfo.username;
+        document.getElementById('avatar').src = `https://cdn.discordapp.com/avatars/${userData.userInfo.id}/${userData.userInfo.avatar}.png`;
+        document.getElementById('email').innerText = userData.userInfo.email;
+
+        // Отображение информации о гильдиях пользователя
+        const guildsContainer = document.getElementById('guilds');
+        userData.userGuilds.forEach(guild => {
+            const guildElement = document.createElement('li');
+            guildElement.innerText = guild.name;
+            guildsContainer.appendChild(guildElement);
+        });
+    })
+    .catch(error => {
+        console.error('Ошибка при получении данных пользователя:', error.message);
+        // Здесь вы можете перенаправить пользователя на страницу логина или показать сообщение
+        alert('Ошибка: ' + error.message);
+    });
+} else {
+    // Если токена нет, перенаправляем на страницу логина
+    window.location.href = "https://forza-event-manager-5e2b3b51921f.herokuapp.com/login";
+}
